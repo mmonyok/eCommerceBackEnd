998e81c8c1697c92e481e49872f5270fc7dc6d5f
Ody <melodymonyok@gmail.com>
2021-07-12 23:17:44 -0500

    Updated the process.env.DB_PW to .DB_PASS.

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
