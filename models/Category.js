794c70962d99fede11ee136fd467689bcfcea785
Ody <melodymonyok@gmail.com>
2021-07-14 11:21:35 -0500

    Added Category Model columns.

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,21 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    Shirts: {
+      type: DataTypes.STRING
+    },
+    Shorts: {
+      type: DataTypes.STRING
+    },
+    Music: {
+      type: DataTypes.STRING
+    },
+    Hats: {
+      type: DataTypes.STRING
+    },
+    Shoes: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
