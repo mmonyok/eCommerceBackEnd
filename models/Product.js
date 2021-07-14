e5a8aa961a8eed73d424803f479d7f8757c29578
Ody <melodymonyok@gmail.com>
2021-07-14 11:33:28 -0500

    Added Product Model columns.

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,18 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2)
+    },
+    stock: {
+      type: DataTypes.INTEGER
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+    } 
   },
   {
     sequelize,
