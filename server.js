5d7203bb23fd0da379ca67e8d88b9804fc467d20
Ody <melodymonyok@gmail.com>
2021-07-14 15:37:55 -0500

    Updated server.js file to connect to sequelize and to sync the connection before running server.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// This statement will connect to the database before starting the Express.js server.
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
