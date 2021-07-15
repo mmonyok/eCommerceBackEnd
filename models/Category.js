9d2cd035b8b72b939b388d7b35d0f74ed707e9cd
Ody <melodymonyok@gmail.com>
2021-07-14 19:02:14 -0500

    Updated file to include an id column.

--- models/Category.js
+++ models/Category.js
@@ -6,8 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     category_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   {
