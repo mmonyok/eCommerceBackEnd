0e18998fb838f6d796c6bbdd2b3522991f8c8f75
Ody <melodymonyok@gmail.com>
2021-07-14 19:18:30 -0500

    Updated file to include missing attributes.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,12 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_id: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
     tag_id: {
-      type: DataTypes.INTEGER
-    }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
