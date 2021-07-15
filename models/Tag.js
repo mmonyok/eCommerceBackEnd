2cf399d3acb5c4878bea3e8dcc5fb7fb3863a720
Ody <melodymonyok@gmail.com>
2021-07-14 19:12:27 -0500

    Updated file to include missing column of id.

--- models/Tag.js
+++ models/Tag.js
@@ -6,9 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     tag_name: {
-      type: DataTypes.STRING
-    }
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
